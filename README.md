1739e60fad5b679f34c535baad029046a9a0c3d0
Ultronxr <Ultronxr@users.noreply.github.com>
2019-01-03 09:12:28 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Student Status Management System
 # 课程设计作业——学籍管理系统
+
+## 不包含 配置文件 和 /target/、/test/文件夹
--- src/test/JDBCTest.java
+++ src/test/JDBCTest.java
@@ -3,11 +3,6 @@ import utils.MysqlUtils;
 import java.sql.Connection;
 
 public class JDBCTest {
-    /***
-     * 阿里云防火墙设置3306开放
-     * /etc/mysql/mysql.conf.d/mysqld.cnf注释bind-host一行
-     * https://blog.csdn.net/qq_39781497/article/details/81302950
-     */
 
     public static void main(String[] args){
 
