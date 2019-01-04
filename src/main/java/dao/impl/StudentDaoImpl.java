7c91eddb748f236950f7a5c0b4b95a6282095a5e
xukaiyue <m15057153163@163.com>
2019-01-04 08:16:36 +0800

    xukaiyue

--- src/main/java/dao/impl/StudentDaoImpl.java
+++ src/main/java/dao/impl/StudentDaoImpl.java
@@ -50,6 +50,41 @@ public class StudentDaoImpl implements StudentDao {
         }
         return student;
     }
+    public Student getStudentInfoByName(String name){
+        Student student = null;
+        String sql="SELECT * FROM Student WHERE name=?";
+        Connection con = MysqlUtils.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setString(1, name);
+            rs = ps.executeQuery();
+            if(rs.next()){
+                student = new Student();
+                student.setId(rs.getString("id"));
+                student.setName(rs.getString("name"));
+                student.setAge(rs.getInt("age"));
+                student.setSex(rs.getString("sex"));
+                student.setInstitute(rs.getString("institute"));
+                student.setMajor(rs.getString("major"));
+                student.setStudentClass(rs.getString("studentClass"));
+                student.setBirthday(rs.getString("birthday"));
+                student.setStartTime(rs.getString("startTime"));
+                student.setEndTIme(rs.getString("endTime"));
+                student.setCredit(rs.getInt("credit"));
+                student.setSource(rs.getString("source"));
+                student.setNationality(rs.getString("nationality"));
+                student.setType(rs.getString("type"));
+                student.setPoliticalStatus(rs.getString("politicalStatus"));
+                student.setGpa(rs.getDouble("gpa"));
+            }
 
-
+        }catch (SQLException e){
+            System.out.println("[x] src.main.java.dao.impl-StudentDaoImpl 通过姓名获取学生实体类时，连接数据库出错！");
+        }finally {
+            MysqlUtils.closeConnection(rs, ps, con);
+        }
+        return student;
+    }
 }
--- src/main/java/entity/Course.java
+++ src/main/java/entity/Course.java
@@ -1,5 +1,94 @@
 package entity;
 
 public class Course {
+    int id;
+    String school_year;
+    int semester;
+    String name;
+    String institute;
+    String category;
+    double credit;
+    String teacher;
+    String class_time;
+    String class_place;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSchool_year() {
+        return school_year;
+    }
+
+    public void setSchool_year(String school_year) {
+        this.school_year = school_year;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInstitute() {
+        return institute;
+    }
+
+    public void setInstitute(String institute) {
+        this.institute = institute;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void setCredit(double credit) {
+        this.credit = credit;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getClass_time() {
+        return class_time;
+    }
+
+    public void setClass_time(String class_time) {
+        this.class_time = class_time;
+    }
+
+    public String getClass_place() {
+        return class_place;
+    }
+
+    public void setClass_place(String class_place) {
+        this.class_place = class_place;
+    }
 }
