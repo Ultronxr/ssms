bdc09297eb14f70ba3056efadc4d89d21c1f666c
xukaiyue <m15057153163@163.com>
2019-01-11 05:49:43 +0800

    xukaiyue

--- src/main/java/action/HandleStudentStatus.java
+++ src/main/java/action/HandleStudentStatus.java
@@ -1,5 +1,9 @@
 package action;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.sun.nio.sctp.HandlerResult;
 import dao.DocDao;
 import dao.StudentDao;
 import dao.impl.DocDaoImpl;
@@ -12,6 +16,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -29,6 +34,18 @@ public class HandleStudentStatus extends HttpServlet {
         String sex;
         int num;
         String result;
+        public HandleStudent(String id, String name, String studentType, String institute, String major, String grade, String studentClass, String sex, int num, String result) {
+            this.id = id;
+            this.name = name;
+            this.studentType = studentType;
+            this.institute = institute;
+            Major = major;
+            this.grade = grade;
+            this.studentClass = studentClass;
+            this.sex = sex;
+            this.num = num;
+            this.result = result;
+        }
     }
     private List<StudentGrade>list=null;
     public String getGradeById(String id){
@@ -40,6 +57,11 @@ public class HandleStudentStatus extends HttpServlet {
         return null;
     }
     public StudentGrade getSGById(String id){
+        for(StudentGrade sg:list){
+            if (sg.getStudentId().equals(id)){
+                return sg;
+            }
+        }
         return null;
     }
     public boolean isgraduate(String grade){
@@ -56,7 +78,7 @@ public class HandleStudentStatus extends HttpServlet {
         //获取所有sc ->list
         DocDao dd=new DocDaoImpl();
         list=dd.getStudentGrade();
-        List<HandleStudent>All=new ArrayList<HandleStudent>();
+        List<HandleStudent>handleResult=new ArrayList<HandleStudent>();
         //转化map id为key
         Map<String,Integer>id_num=new HashMap<String,Integer>();
         for(StudentGrade sg:list){
@@ -68,42 +90,69 @@ public class HandleStudentStatus extends HttpServlet {
                 id_num.put(sg.getStudentId(),0);
             }
         }
-
         //处理不及格数的学生
         StudentDao sd=new StudentDaoImpl();
         for (String key : id_num.keySet()){
+            StudentGrade sg=getSGById(key);
             //退学5
             if(id_num.get(key)>5){
                 sd.updateStatus(key,"退学");
+                HandleStudent hs=new HandleStudent(key,sg.getStudentName(),sg.getStudentType(),sg.getInstitute(),sg.getMajor(),sg.getGrade(),sg.getStudentClass(),sg.getSex(),id_num.get(key),"退学");
+                handleResult.add(hs);
             }
             //不及格3-4
             else if(id_num.get(key)>=3&&id_num.get(key)<5){
                 if(!isgraduate(getGradeById(key))) {
                     sd.updateStatus(key, "警示");
+                    HandleStudent hs=new HandleStudent(key,sg.getStudentName(),sg.getStudentType(),sg.getInstitute(),sg.getMajor(),sg.getGrade(),sg.getStudentClass(),sg.getSex(),id_num.get(key),"警示");
+                    handleResult.add(hs);
                 }else{
                     sd.updateStatus(key,"留级");
+                    HandleStudent hs=new HandleStudent(key,sg.getStudentName(),sg.getStudentType(),sg.getInstitute(),sg.getMajor(),sg.getGrade(),sg.getStudentClass(),sg.getSex(),id_num.get(key),"留级");
+                    handleResult.add(hs);
                 }
             }
             //优秀0
             else if(id_num.get(key)==0){
                 if(!isgraduate(getGradeById(key))) {
                     sd.updateStatus(key, "优秀");
+                    HandleStudent hs=new HandleStudent(key,sg.getStudentName(),sg.getStudentType(),sg.getInstitute(),sg.getMajor(),sg.getGrade(),sg.getStudentClass(),sg.getSex(),id_num.get(key),"优秀");
+                    handleResult.add(hs);
                 }else{
                     sd.updateStatus(key,"毕业");
+                    HandleStudent hs=new HandleStudent(key,sg.getStudentName(),sg.getStudentType(),sg.getInstitute(),sg.getMajor(),sg.getGrade(),sg.getStudentClass(),sg.getSex(),id_num.get(key),"毕业");
+                    handleResult.add(hs);
                 }
             }//及格1-2
             else{
                 if(!isgraduate(getGradeById(key))) {
                     sd.updateStatus(key,"及格");
+                    HandleStudent hs=new HandleStudent(key,sg.getStudentName(),sg.getStudentType(),sg.getInstitute(),sg.getMajor(),sg.getGrade(),sg.getStudentClass(),sg.getSex(),id_num.get(key),"及格");
+                    handleResult.add(hs);
                 }else{
                     sd.updateStatus(key,"毕业");
+                    HandleStudent hs=new HandleStudent(key,sg.getStudentName(),sg.getStudentType(),sg.getInstitute(),sg.getMajor(),sg.getGrade(),sg.getStudentClass(),sg.getSex(),id_num.get(key),"毕业");
+                    handleResult.add(hs);
                 }
             }
         }
+        JSONArray array = JSONArray.parseArray(JSON.toJSONString(handleResult));
+        JSONObject json = new JSONObject();
+        json.put("data", array);
+        output(response,json);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request,response);
     }
-
+    private void output(HttpServletResponse response, JSONObject json) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setDateHeader("Expires", 0);
+        response.setHeader("Cache-Control","no-cache");
+        response.setHeader("Pragma","no-cache");
+        PrintWriter out = response.getWriter();
+        out.println(json.toJSONString());
+        out.flush();
+        out.close();
+    }
 }
